653c9f571c8ee5abfd9656799eb251cf712c3221
Isaac Yoon <isaachyoon@fb.com>
2019-12-12 19:49:02 -0800

    .

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import './Person.css';
 
-function Person(props) { //prop:{id:'', name:''}
-  return (
-      <div className="person">
-        <p>{props.data.id}</p>
-        <p>{props.data.name}</p>
-      </div>
-  );
+class Person extends React.Component { 
+  constructor(props) {
+    super(props);
+    console.log(props)
+    this.state = {
+      like: props.data.like
+    }
+  }
+  componentDidMount = () => {
+    console.log('componentDidMount()');
+  } // Called after component mounts
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate()');
+  } // Called after each render
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  } // Called before component unmounts
+
+  clickHandler(id) {
+    let like = this.state.like;
+    this.setState({like: like+1});
+  }
+
+  render() {
+    return (
+        <div className="person">
+          <p>{this.props.data.id}</p>
+          <p>{this.props.data.name}</p>
+          {/* {props.children} */}
+          <button onClick={() => this.clickHandler()}>like</button>
+          <span>{this.state.like}</span>
+        </div>
+    );
+  }
 };
 
 Person.defaultProps = {
--- src/PersonList/PersonList.js
+++ src/PersonList/PersonList.js
@@ -2,28 +2,61 @@ import React from 'react';
 import Person from '../Person/Person';
 import './PersonList.css';
 
-function PersonList() {
-  const peopleData = [
-      { id: '001', name: 'Rich'},
-      { id: '002', name: 'Joe' },
-      { id: '003', name: 'Chris'}, 
-      { id: '004', name: 'Tauhida' },
-      { id: '005', name: 'Elias' }, 
-      { id: '006', name: 'Rita' }
-  ];
+let peopleData = [
+  { id: '001', name: 'Rich', like: 0},
+  { id: '002', name: 'Joe', like: 0},
+  { id: '003', name: 'Chris', like: 0}, 
+  { id: '004', name: 'Tauhida', like: 0},
+  { id: '005', name: 'Elias', like: 0}, 
+  { id: '006', name: 'Rita', like: 0}
+];
 
-  const people = peopleData.map((person, i) =>
-      <Person key={i} />
-  );
-  
-  return (
-    <div className="personList">
-      <h3>Person List</h3>
-      <div className="container">
-        {people}
+class PersonList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: peopleData
+    }
+  }
+
+
+  clickHandler(id) {
+    console.log(id)
+    const newData = peopleData.map((person) => {
+      if (person.id === id) {
+        person.like += 1;
+        console.log(person.like)
+      }
+    })
+    this.setState({data: newData});
+  }
+
+  deleteLastComponent() {
+    console.log('hi');
+    peopleData = peopleData.slice();
+    peopleData.pop();
+    console.log(peopleData);
+    this.setState({data: peopleData});
+  }
+
+  //this.peopleData
+  render() {
+    const people = this.state.data.map((person, i) =>
+      <Person key={i} data={person}>
+        {/* <button onClick={() => this.clickHandler(person.id)}>like</button> */}
+        {/* <span>{person.like}</span> */}
+      </Person>
+    );
+    return (
+      <div className="personList">
+        <h3>Person List</h3>
+        <div className="container">
+          {people}
+        </div>
+        <button onClick={() => this.deleteLastComponent()}>Delete</button>
       </div>
-    </div>
-  );
+    )
+  }
 };
 
 export default PersonList;
